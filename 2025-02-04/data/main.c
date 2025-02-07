fe3cf10d998cc537daf81cc0d2127347d8d46f17
TomAintAround <tcorreia2005@proton.me>
2025-02-07 12:04:13 +0000

    feat(2025-02-04/data): ask for the name aswell

--- 2025-02-04/data/main.c
+++ 2025-02-04/data/main.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 
 int main() {
+	char nome[100];
 	int idade;
 	int peso;
 	
+	printf("O seu nome: ");
+	if (scanf("%s", nome) != 1) {
+		printf("\nAlgo correu mal");
+		return -1;
+	}
 	printf("A sua idade: ");
 	if (scanf("%d", &idade) != 1) {
-		printf("Algo correu mal");
+		printf("\nAlgo correu mal");
+		return -1;
 	}
 	printf("O seu peso: ");
 	if (scanf("%d", &peso) != 1) {
-		printf("Algo correu mal");
+		printf("\nAlgo correu mal");
+		return -1;
 	}
 
-	printf("Chamo-me Tomás, tenho %d anos e peso %d Kg.\n", idade, peso);
+	printf("Chamo-me %s, tenho %d anos e peso %d Kg.\n", nome, idade, peso);
 	return 0;
 }
