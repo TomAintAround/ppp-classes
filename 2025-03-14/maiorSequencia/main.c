e72d3877bf0aab385f8be97c9aecbc06763fcf41
TomAintAround <tcorreia2005@proton.me>
2025-03-18 14:24:45 +0000

    perf(2025-03-14/maiorSequencia): change method for detecting biggest number

--- 2025-03-14/maiorSequencia/main.c
+++ 2025-03-14/maiorSequencia/main.c
@@ -2,7 +2,6 @@
 // Exercício 10.3.1
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "main.h"
 
 int eNumero(char letra) {
@@ -17,34 +16,22 @@ int main() {
 		return -1;
 	}
 
-	int maiorSequencia = 0, sequencia = 0;
-	int maiorNumeroInicial = 0, numeroInicial = 0;
-	int maiorNumeroFinal = 0, numeroFinal = 0;
-	for (int i = 0; i < (int)strlen(string); i++) {
-		if (eNumero(string[i])) {
-			sequencia++;
-			numeroFinal = i;
-			if (i > 0 && !eNumero(string[i - 1])) {
-				numeroInicial = i;
-			}
-		}
-		if (sequencia > maiorSequencia) {
-			maiorSequencia = sequencia;
-			maiorNumeroInicial = numeroInicial;
-			maiorNumeroFinal = numeroFinal;
-		}
-		if (!eNumero(string[i]) && sequencia > 0) sequencia = 0;
-	}
-
-	if (maiorSequencia > 0) {
-		long numero = 0;
-		for (int i = maiorNumeroInicial; i <= maiorNumeroFinal; i++) {
+	long numeroMaior = 0; long numero = 0;
+	for (int i = 0; i <= MAX; i++) {
+		if (string[i] >= '0' && string[i] <= '9') {
 			numero *= 10;
 			numero += string[i] - '0';
 		}
-		printf("Inteiro: %ld\n", numero);
+		else numero = 0;
+		if (numero > numeroMaior) numeroMaior = numero;
 	}
-	else printf("Não foi introduzido um inteiro.\n");
+
+	if (numeroMaior == 0) {
+		printf("Não foi encontrado um inteiro.\n");
+	}
+	else {
+		printf("Inteiro: %ld\n", numeroMaior);
+	}
 
 	free(string);
 	return 0;
